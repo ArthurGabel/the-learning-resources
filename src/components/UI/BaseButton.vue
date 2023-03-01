55b00d297a7b61fde9e2ef7a6d97968defb00c4a
Arthur Gäbel <arthurgabel@hotmail.com>
2023-03-01 08:47:22 -0300

    BaseButton component add

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button :class="mode" :type="type">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['type', 'mode']
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  margin: 0 0.5rem 0 0;
+  font-family: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  border-radius: 12px;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+</style>
